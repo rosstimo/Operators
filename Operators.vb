4b8d189dd790a3474f5ca4d900eedb5dcdeab7f5
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-08-19 08:45:15 -0600

    Add project files.

---
+++ Operators.vb
@@ -0,0 +1,29 @@
+﻿'Tim Rossiter
+'RCET0265
+'Fall 2020
+'Operators Example
+'repo
+
+
+Module Operators
+
+    Sub Main()
+        'Let's do math!!
+        'addition
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+        Console.WriteLine(7 - 1)
+        Console.ReadLine()
+        Console.WriteLine(5 * 5)
+        Console.ReadLine()
+        Console.WriteLine(7 / 3)
+        Console.ReadLine()
+        Console.WriteLine(7 \ 3)
+        Console.ReadLine()
+        Console.WriteLine(47.9 Mod 9.35)
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
