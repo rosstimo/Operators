24fb9b360a139fc48e49101f8bdc0414b736f397
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-08-20 08:22:00 -0600

    add concatenation examples

--- Operators.vb
+++ Operators.vb
@@ -10,18 +10,32 @@ Module Operators
     Sub Main()
         'Let's do math!!
         'addition
-        Console.WriteLine(2 + 2)
+        'Console.WriteLine(2 + 2)
+        'Console.ReadLine()
+        'Console.WriteLine(7 - 1)
+        'Console.ReadLine()
+        'Console.WriteLine(5 * 5)
+        'Console.ReadLine()
+        'Console.WriteLine(7 / 3)
+        'Console.ReadLine()
+        'Console.WriteLine(7 \ 3)
+        'Console.ReadLine()
+        'Console.WriteLine(47.9 Mod 9.35)
+        'Console.ReadLine()
+
+
+        'concatenate stuff
+        'use with caution
+        Console.WriteLine("2" + "2")
         Console.ReadLine()
-        Console.WriteLine(7 - 1)
+        'happy times
+        Console.WriteLine("2" & "2")
         Console.ReadLine()
-        Console.WriteLine(5 * 5)
+        'example
+        Console.WriteLine("Hello" & "," & " " & "World!")
         Console.ReadLine()
-        Console.WriteLine(7 / 3)
-        Console.ReadLine()
-        Console.WriteLine(7 \ 3)
-        Console.ReadLine()
-        Console.WriteLine(47.9 Mod 9.35)
-        Console.ReadLine()
+
+
 
 
     End Sub
